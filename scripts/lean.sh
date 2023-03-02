9de064fdd8e05c4338bb3cd00c74d96a0f7882c3
MilesPoupart <renjunyang007@126.com>
2023-03-02 22:47:28 +0800

    passwall test

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -57,9 +57,9 @@ rm -rf ../../customfeeds/packages/net/adguardhome
 
 # Add luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall
-cd openwrt-passwall
-git checkout 4fd4bf8
-cd ../
+# cd openwrt-passwall
+# git checkout 4fd4bf8
+# cd ../
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 
